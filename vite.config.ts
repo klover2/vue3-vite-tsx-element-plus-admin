42593d1898ac78b52c815f0e775a5523e9054dec
klover <49420645@qq.com>
2022-11-04 00:03:45 +0800

    修改element-plus组件不显示问题

--- vite.config.ts
+++ vite.config.ts
@@ -61,19 +61,9 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       chunkSizeWarningLimit: 700,
       rollupOptions: {
         output: {
-          // 最小化拆分包
-          manualChunks: (id) => {
-            if (id.includes("node_modules")) {
-              return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString();
-            }
-          }, // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
-          entryFileNames: "js/[name].[hash].js", // 用于命名代码拆分时创建的共享块的输出命名
-          chunkFileNames: "js/[name].[hash].js", // 用于输出静态资源的命名，[ext]表示文件扩展名
-          assetFileNames: "[ext]/[name].[hash].[ext]", // 拆分js到模块文件夹
+          manualChunks: {
+            "element-plus": ["element-plus"],
+          },
         },
       },
     },
